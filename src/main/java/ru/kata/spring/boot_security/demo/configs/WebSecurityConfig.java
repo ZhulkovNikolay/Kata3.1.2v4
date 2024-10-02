69a65f1c3c304e9a31780bc0193dea34c988db3d
ZhulkovNikolay <wed10@yandex.ru>
2024-10-02 20:19:08 +0300

    PreAuthorize

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -2,7 +2,7 @@ package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,11 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import ru.kata.spring.boot_security.demo.model.Permission;
 import ru.kata.spring.boot_security.demo.model.Role;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SuccessUserHandler successUserHandler;
 
@@ -30,9 +30,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/index").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.USERS_READ.getPermission())
-                .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.USERS_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.USERS_WRITE.getPermission())
+             // .antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.USERS_READ.getPermission())
+             // .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.USERS_WRITE.getPermission())
+             // .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.USERS_WRITE.getPermission())
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().successHandler(successUserHandler)
--- src/main/java/ru/kata/spring/boot_security/demo/rest/UserRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/rest/UserRestController.java
@@ -1,5 +1,6 @@
 package ru.kata.spring.boot_security.demo.rest;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 
@@ -23,18 +24,21 @@ public class UserRestController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('users:read')")
     public User getById(@PathVariable Long id) {
         return USERS.stream().filter(user -> user.getId().equals(id))
                 .findFirst().orElse(null);
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('users:write')")
     public User create(@RequestBody User user) {
         this.USERS.add(user);
         return user;
     }
 
     @DeleteMapping
+    @PreAuthorize("hasAuthority('users:write')")
     public void deleteById(@PathVariable Long id) {
         this.USERS.removeIf(user -> user.getId().equals(id));
     }
